2a42fc5186b4cf70ccf1b03eb241130a9599321b
LAKSHYA KUMAR <smartlakshyakumar@gmail.com>
2022-04-05 19:12:04 +0530

    PHP,  SQL Database added

--- Signup/insert.php
+++ Signup/insert.php
@@ -1,9 +1,57 @@
 <?php
 
-$fullName = $_POST['FnameFromUser'];
-$Password = $_POST['passwordFromUser'];
-$PhoneNumber = $_POST["phoneFromUser"];
-$Email = $_POST["emailFromUser"];
-$Age = $_POST["ageFromUser"];
-$Gender = $_POST["genderFromUser"];
+if (isset($_POST['submit'])) {
+    if (isset($_POST['username']) && isset($_POST['password']) &&
+        isset($_POST['gender']) && isset($_POST['email']) &&
+        isset($_POST['phoneCode']) && isset($_POST['phone'])) {
+
+
+        $fullName = $_POST['FnameFromUser'];
+        $Password = $_POST['passwordFromUser'];
+        $PhoneNumber = $_POST["phoneFromUser"];
+        $Email = $_POST["emailFromUser"];
+        $Age = $_POST["ageFromUser"];
+        $Gender = $_POST["genderFromUser"];
+
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "CreateAccount";
+
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        } else {
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(fullName, Password, PhoneNumber, Email, Age, Gender) values(?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+
+            if ($rnum == 0) {
+                $stmt->close();
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssssii", $fullName, $Password, $PhoneNumber, $Email, $Age, $Gender);
+                if ($stmt->execute()) {
+                    echo "New record inserted successfully.";
+                } else {
+                    echo $stmt->error;
+                }
+            } else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    } else {
+        echo "All field are required.";
+        die();
+    }
+} else {
+    echo "Submit button is not set";
+}
 
