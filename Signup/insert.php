1f50b8aa67bfda42b8934236b7df72e56eef2ee0
LAKSHYA KUMAR <smartlakshyakumar@gmail.com>
2022-04-05 18:29:52 +0530

    A lot has been changed

---
+++ Signup/insert.php
@@ -0,0 +1,9 @@
+<?php
+
+$fullName = $_POST['FnameFromUser'];
+$Password = $_POST['passwordFromUser'];
+$PhoneNumber = $_POST["phoneFromUser"];
+$Email = $_POST["emailFromUser"];
+$Age = $_POST["ageFromUser"];
+$Gender = $_POST["genderFromUser"];
+
